db453c849166b45bd23627b146fa3428bd0b5f9c
varshini <varshinisugavanesh@gmail.com>
2024-10-11 19:48:00 +0530

    added a custom hooks to fetch the data

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
 
 const MovieList = ({ apiPath }) => {
-  return <main className="min-h-screen">{apiPath}</main>;
+  const { data: movies } = useFetch(apiPath);
+  console.log(movies);
+  return (
+    <>
+      <main className="min-h-screen">
+        {movies.map((movie) => (
+          <h1>{movie.original_title}</h1>
+        ))}
+      </main>
+      ;
+    </>
+  );
 };
 
 export default MovieList;
