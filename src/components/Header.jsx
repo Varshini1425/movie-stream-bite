7e35e16a953ae96bdfda063f1cfe628da55c2537
varshini <varshinisugavanesh@gmail.com>
2024-10-13 20:34:08 +0530

    added a logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
             <img
-              src="https://flowbite.com/docs/images/logo.svg"
+              src="./movieBiteIcon.svg"
               className="h-8"
               alt="Flowbite Logo"
             />
