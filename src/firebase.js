5e60d32da829030c11859e0464af70b860164ec0
varshini <varshinisugavanesh@gmail.com>
2024-10-13 19:39:53 +0530

    added a firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtqtxRjRJ14ZTDTZZmx-2uLIJfzXNjVAs",
+  authDomain: "moviebite-8ab46.firebaseapp.com",
+  projectId: "moviebite-8ab46",
+  storageBucket: "moviebite-8ab46.appspot.com",
+  messagingSenderId: "222225614229",
+  appId: "1:222225614229:web:a7146c701823c0c6e7629c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
