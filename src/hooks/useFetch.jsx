ddd7f1e440fa03e1d7690fa37c800e470189d839
varshini <varshinisugavanesh@gmail.com>
2024-10-21 20:18:48 +0530

    modified the env variables

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 import { options } from "../utils/Options";
 
-const useFetch = (apiPath, queryTerm) => {
+const useFetch = (apiPath) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     async function fetchMovies() {
       const response = await fetch(
-        `http://api.themoviedb.org/3/${apiPath}?language=en-US&page=1`,
+        `https://api.themoviedb.org/3/${apiPath}?language=en-US&page=1`,
         options
       );
       const data = await response.json();
